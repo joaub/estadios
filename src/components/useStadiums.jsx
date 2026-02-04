d2272c087f756e56cc4d040d10bac07d758fd190
Joaquin Ugarte <ugartejoa12@gmail.com>
2026-02-04 16:07:57 -0300

    first commit

---
+++ src/components/useStadiums.jsx
@@ -0,0 +1,12 @@
+import {useState, useEffect} from "react";
+
+const useStadiums = () => {
+    const [stadiums, setStadiums] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+    }, [])
+}
+export default useStadiums;
