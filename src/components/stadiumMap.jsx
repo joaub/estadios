5795c53969d3ca771eac602b8c5b44a5d690faec
Joaquin Ugarte <ugartejoa12@gmail.com>
2026-02-10 15:46:45 -0300

    intentando agregar estadios

--- src/components/stadiumMap.jsx
+++ src/components/stadiumMap.jsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useStadiums } from './useStadiums';
+
 import { useState } from 'react';
 
 const StadiumMap = () => {
@@ -10,19 +11,65 @@ const StadiumMap = () => {
     const [newStadium, setNewStadium] = useState(null);
     const [customStadiums, setCustomStadiums] = useState([]);
 
+    const saveStadium = () => {
+        setCustomStadiums([...customStadiums, newStadium]);
+        setNewStadium(null);
+        setAddMode(false);
+    };
+
     return (
         <div className="map-wrapper">
             {loading && <p className="status">Cargando estadios...</p>}
             {error && <p className="status error">{error}</p>}
 
-            <button 
-            onClick={() => setAddMode(!addMode)}>
-                { addMode ? "cancelar" : " agregar estadios"}
+            <button
+                onClick={() => setAddMode(!addMode)}
+                style={{
+                    position: "absolute",
+                    zIndex: 1000,
+                    top: 10,
+                    left: 50,
+                    padding: "8px 12px"
+                }}>
+                {addMode ? "cancelar" : " agregar estadios"}
             </button>
+            {newStadium && (
+                <div style={{
+                    position: "absolute",
+                    zIndex: 1000,
+                    bottom: 20,
+                    left: 20,
+                    background: "white",
+                    padding: 12,
+                    borderRadius: 8,
+                    width: 250
+                }}>
+                    <h4>Nuevo Estadio</h4>
+
+                    <input type="text" placeholder="Nombre del estadio"
+                        onChange={(e) =>
+                            setNewStadium({ ...newStadium, name: e.target.value })} />
+                    <input type="text" placeholder='Equipo'
+                        onChange={(e) =>
+                            setNewStadium({ ...newStadium, team: e.target.value })
+                        } />
+                    <button onClick={saveStadium}>Guardar</button>
+                </div>
+            )}
             <MapContainer
                 center={[-32.8895, -68.8458]}
                 zoom={9}
                 className="leaflet-map"
+                whenReady={(map) => {
+                    map.target.on("Click", (e) => {
+                        if (!addMode) return;
+
+                        setNewStadium({
+                            lat: e.latlng.lat,
+                            lon: e.latlng.lng
+                        });
+                    })
+                }}
             >
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -41,8 +88,26 @@ const StadiumMap = () => {
                                 </Popup>
                             </Marker>
                         );
+
                     })}
+                {customStadiums.map((stadium, index) => (
+                    <Marker
+                        key={`custom-${index}`}
+                        position={[stadium.lat, stadium.lon]}
+                    >
+                        <Popup>
+                            <strong>{stadium.name}</strong>
+                            <br />
+                            {stadium.team}
+                            <br />
+                            {stadium.crest && (
+                                <img src={stadium.crest} width={40} />
+                            )}
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
+            
         </div>
     );
 };
